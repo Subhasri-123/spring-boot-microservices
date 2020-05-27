7f53c49ae6db958431be0a75799eaed9edeff6ef
subhasri_sahoo <subhasri_sahoo@THBSGVLPT-0498.thbs.india.com>
2020-05-27 03:56:11 -0600

    DoneCatalog

---
+++ src/main/java/com/example/demo/controller/MovieCatalogController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+
+	@GetMapping("/{userId}")
+	public List<CatalogItem> getCatalogItem(@PathVariable("userId") String userId) {
+		
+		return Collections.singletonList(new CatalogItem("Titanic","Test",4));
+	}
+	
+}
