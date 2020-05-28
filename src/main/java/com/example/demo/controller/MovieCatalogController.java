be18d2dbb45e10b4504526848660d0cc7ce1b2f5
subhasri_sahoo <subhasri_sahoo@THBSGVLPT-0498.thbs.india.com>
2020-05-28 06:31:08 -0600

    Rest & Web

--- src/main/java/com/example/demo/controller/MovieCatalogController.java
+++ src/main/java/com/example/demo/controller/MovieCatalogController.java
@@ -1,23 +1,52 @@
 package com.example.demo.controller;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.demo.model.CatalogItem;
+import com.example.demo.model.Movie;
+import com.example.demo.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder builder;
 
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalogItem(@PathVariable("userId") String userId) {
 		
-		return Collections.singletonList(new CatalogItem("Titanic","Test",4));//
+		
+		//Movie movie = restTemplate.getForObject("http://localhost:8083/movies/foo", Movie.class);
+		
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 5),
+				new Rating("5678", 4)
+				);
+		
+		return ratings.stream().map(rating -> {
+			 Movie movie = restTemplate.getForObject("http://localhost:8083/movies/" + rating.getMovieId(),
+					Movie.class);
+			/*
+			 * Movie movie = builder.build() .get() .uri("http://localhost:8083/movies/" +
+			 * rating.getMovieId()) .retrieve() .bodyToMono(Movie.class) .block();
+			 */
+			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+		}).collect(Collectors.toList());
+		//return Collections.singletonList(new CatalogItem("Titanic","Test",4));
 	}
 	
 }
